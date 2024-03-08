617e447ab4ca998c3113dbe2fc90a1239e112388
AdebayoMarzouq <marzouqdevmail@gmail.com>
2024-03-08 10:55:39 +0100

    Initial comit

--- console.py
+++ console.py
@@ -34,9 +34,11 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def parseline(self, line):
-        pattern = r"^(.*?)\.(.*?)\((.*?)\)"
+        pattern = r"(\b\w+)\.(\w+)\(([^()]*)\)"
         matches = re.findall(pattern, line)
         if matches and len(matches[0]) == 3:
+            method_name = matches[0][1]
+            model_name = matches[0][0]
             arguments = ""
             pattern = r"\{.*\}"
             match = re.search(pattern, line)
@@ -49,16 +51,16 @@ class HBNBCommand(cmd.Cmd):
                         arr.append(k)
                         arr.append(str(v).strip())
                     arguments = pre[0].replace('"', "") + " " + " ".join(arr)
+                    method_name = "dict_" + method_name
                 except json.decoder.JSONDecodeError:
-                    
-                    return (None, None, "")
+                    return cmd.Cmd.parseline(self, line)
             else:
                 pre = re.split(r",\s*", matches[0][2])
-                arguments = " ".join(pre)
+                arguments = " ".join(pre).replace('"', "")
             combined = (
-                matches[0][1]
+                method_name
                 + " " +
-                matches[0][0]
+                model_name
                 + " " +
                 arguments)
             return (None, None, combined)
@@ -66,7 +68,6 @@ class HBNBCommand(cmd.Cmd):
         return ret
 
     def default(self, line):
-        print(line)
         args = line.split(' ')
         if len(args) < 3:
             return cmd.Cmd.default(self, line)
@@ -77,6 +78,7 @@ class HBNBCommand(cmd.Cmd):
             _cls(" ".join(final_args))
         except AttributeError:
             return cmd.Cmd.default(self, line)
+        return None
 
     def do_count(self, line):
         """Counts all instances of class class name.
@@ -120,46 +122,76 @@ class HBNBCommand(cmd.Cmd):
         Args:
             line (str): model name, model id, attribute name, attribute value
         """
+        errors = {
+            0: "** class name missing **",
+            1: "** instance id missing **",
+            2: "** attribute name missing **",
+            3: "** value missing **"
+        }
         args = line.split()
         if len(args) == 0:
-            print("** class name missing **")
-            return
+            print(errors[0])
+            return False
         model_name = args[0]
         if model_name not in HBNBCommand.all_classes:
             print("** class doesn't exist **")
-            return
+            return False
         if len(args) == 1:
-            print("** instance id missing **")
-            return
+            print(errors[1])
+            return False
         model_id = args[1]
         key = model_name + "." + model_id
         all_objs = storage.all()
         if key not in all_objs:
             print("** no instance found **")
-            return
-        if len(args) == 2:
-            print("** attribute name missing **")
-            return
-        if len(args) == 3:
-            print("** value missing **")
-            return
+            return False
+        if len(args) < 4:
+            print(errors[len(args)])
+            return False
         model_name = args[0]
         model_id = args[1]
         attr_name = args[2]
         attr_value = args[3]
         if attr_name in ['id', 'created_at', 'updated_at']:
-            # print(f"{attr_name} can not be updated")
-            return
-        key = model_name + "." + model_id
-        model_obj = all_objs[key]
-        if attr_name not in vars(HBNBCommand.all_classes[model_name]):
-            # print(f"{attr_name} not a class attribute")
-            return
-        # We could replace this attr_type line with json.loads(attr_value)
-        # to convert it to original type
-        attr_type = type(vars(HBNBCommand.all_classes[model_name])[attr_name])
-        setattr(model_obj, attr_name, attr_type(attr_value))
-        model_obj.save()
+            pass
+        else:
+            key = model_name + "." + model_id
+            model_obj = all_objs[key]
+            # if attr_name not in vars(HBNBCommand.all_classes[model_name]):
+            #     # print(f"{attr_name} not a class attribute")
+            #     return
+            # We could replace this attr_type line with json.loads(attr_value)
+            # to convert it to original type
+            # attr_type = type(vars(HBNBCommand.all_classes[model_name])
+            # [attr_name])
+            # setattr(model_obj, attr_name, attr_type(attr_value))
+            try:
+                setattr(model_obj, attr_name, json.loads(
+                    '"' + attr_value + '"'))
+                model_obj.save()
+            except json.JSONDecodeError:
+                return False
+        return True
+
+    def do_dict_update(self, line):
+        # print(line)
+        args = line.split()
+        if len(args) <= 4:
+            return self.do_update(line)
+        model_name = args[0]
+        model_id = args[1]
+        rest = args[2:]
+        while len(rest) > 0:
+            print(rest)
+            my_line = model_name + " " + model_id + " " + " ".join(rest)
+            print(my_line)
+            valid = self.do_update(
+                model_name + " " + model_id + " " + " ".join(rest))
+            print(valid)
+            if not valid:
+                return False
+            rest = rest[2:]
+        return True
 
     def do_destroy(self, line):
         """do_destroy deletes a model instance
