b25c353f57a604af8719f8695d835d9acca2078b
AdebayoMarzouq <marzouqdevmail@gmail.com>
2024-03-08 11:35:23 +0100

    Fixed invalid syntax by reconstructing arguments

--- console.py
+++ console.py
@@ -77,7 +77,8 @@ class HBNBCommand(cmd.Cmd):
             _cls = getattr(self, 'do_' + model_method)
             _cls(" ".join(final_args))
         except AttributeError:
-            return cmd.Cmd.default(self, line)
+            reconstruct_line = f"{args[1]}.{args[0]}({', '.join(args[2:])})"
+            return cmd.Cmd.default(self, reconstruct_line)
         return None
 
     def do_count(self, line):
